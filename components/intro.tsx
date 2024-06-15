e0f6bc44bea697695eee68f36940dc42c30fdeb7
Aditya Panwar <115388093+AdityaRamanSingh@users.noreply.github.com>
2024-06-15 17:34:49 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,7 +92,7 @@ export default function Home() {
                         {/* GOOGLE DRIVE LINK */}
         <a
           className="group bg-white px-7 ml-3 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/17jSESmWcd0jYe43lcKyjGN0CPhVShpUw/view?usp=sharing"
+          href="https://drive.google.com/file/d/1sKU_zq1MrZZhuBZuPDNNpasE0ChIK1vT/view?usp=sharing"
           download
         >
           Download CV{" "}
@@ -102,7 +102,7 @@ export default function Home() {
 
         <a
           className="bg-white p-4 text-gray-700 ml-3 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/aditya-raman-singh-panwar/"
+          href="www.linkedin.com/in/adityaraman19"
           target="_blank"
         >
           <BsLinkedin />
